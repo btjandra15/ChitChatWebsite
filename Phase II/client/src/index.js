68e881a81f8e126970059fa66fe474482e445fe0
btjandra15 <btjandra15@gmail.com>
2023-10-29 23:56:54 -0400

    Updated backend
    - Finished the login & sign up authorization and connected it with the database

--- Phase II/client/src/index.js
+++ Phase II/client/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Auth0Provider domain='loginsystem2.us.auth0.com' clientId='vJR8yCwEAegjaSXnobHaoRcvwroDngyL' authorizationParams={{redirect_uri: window.location.origin}}>
-    <App/>
-  </Auth0Provider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>
 );
