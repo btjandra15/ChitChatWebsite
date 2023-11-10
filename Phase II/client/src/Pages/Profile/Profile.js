c5405a31fbe409a9aca93971e6a91a7c1cbf7b35
Sohail A <107506915+thesameninja@users.noreply.github.com>
2023-11-10 18:29:23 -0500

    Profile Page outline
    * Added structure for profile page.
    * Updated Navbar.css to fix the z-index bug.

--- Phase II/client/src/Pages/Profile/Profile.js
+++ Phase II/client/src/Pages/Profile/Profile.js
@@ -1,9 +1,170 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import "./Profile.scss"; 
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+import { DarkModeContext } from "../../context/darkModeContext";
+import Navbar from "../../components/Navbar/Navbar.js"
+import Leftbar from "../../components/LeftBar/Leftbar";
+import ProfileTimeline from "./components/ProfileTimeline.js"; // New component for profile timeline
+import Rightbar from "../../components/RightBar/Rightbar.js";
+import PostComponent from "../../components/Post/PostComponent";
+
+const cookies = new Cookies();
+const token = cookies.get("TOKEN");
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [userData, setUserData] = useState(null);
+    const [allUserData, setAllUserData] = useState([]);
+    const { darkMode } = useContext(DarkModeContext);
+    const [ postData, setPostData ] = useState([]);
 
-export default Profile
+    const logout = () => {
+        cookies.remove("TOKEN", { path: "/" });
+        setLoggedIn(false);
+    }
+
+    const updateUser = (userId, fieldToUpdateParam, newValueParam) => {
+        axios.put(`http://localhost:3001/update-user/${userId}`, { fieldToUpdate: fieldToUpdateParam, newValue: newValueParam })
+            .then(() => {
+                console.log("Successfuly updated user");
+            })
+            .catch((err) => {
+                console.error(`Error updating User: ${err}`);
+            });
+    }
+
+    useEffect(() => {
+        const loggedInUserConfig = {
+            method: "GET",
+            url: `http://localhost:3001/get-user`,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        const allUserConfig = {
+            method: "GET",
+            url: `http://localhost:3001/get-all-users`,
+        }
+    
+        const postConfig = {
+            method: 'GET',
+            url: 'http://localhost:3001/get-post',
+        }
+
+        //Gets all Users & checks to see if they meet the trendy requirements
+        axios(allUserConfig)
+            .then((res) => {
+                setAllUserData(res.data);
+
+                res.data.map((user) => {
+                    const subscribedUsersCount = user.subscribersList.length;
+                    const likeDislikeDifference = user.likes - user.dislikes;
+                    const trendyMessagesCount = user.trendyMessages.length;
+
+                    if(user.userType !== 'Trendy User'){
+                        if(subscribedUsersCount > 10 && trendyMessagesCount > 2 && (likeDislikeDifference > 10 || user.tips > 100))
+                           updateUser(user._id, 'trendyUser', true);
+                        else
+                            updateUser(user._id, 'trendyUser', false);
+                    }
+
+                    return null;
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
+        //Checks the current logged in user
+        axios(loggedInUserConfig)
+            .then((res) => {
+                setLoggedIn(true);
+                setUserData(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        //Gets all post & checks to see if they meet the trendy requirements
+        axios(postConfig)
+            .then((res) => {
+              setPostData(res.data);
+
+              res.data.map((post) => {
+                const difference = post.likes - post.dislikes;
+                const views = post.views;
+
+                if(difference > 3 && views > 10){
+                    if(post.trendyPost === false){
+                        axios.put(`http://localhost:3001/update-post/${post._id}`, { fieldToUpdate: 'trendyPost', newValue: true })
+                            .then((res) => {
+                                console.log("TrendyPost status updated sucessfully");
+                            })
+                            .catch((err) => {
+                                console.error(`Error updating Trendypost: ${err}`);
+                            })
+                    }
+                }
+
+                return null;
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+    }, [])
+    return(
+        <div className={`theme-${darkMode ? 'dark' : 'light'}`}>
+            {/* NAVBAR CONTENT */}
+            <Navbar loggedIn={loggedIn} userData={userData}/>
+
+            <div className="main-content">
+                {/* LEFTBAR CONTENT */}
+                <Leftbar loggedIn={loggedIn} userData={userData} logout={logout}/>
+
+                {/* MIDDLE CONTENT */}
+                <div style={{flex: 6}}>
+                    <div className='middleBar'>
+                        {loggedIn ? <ProfileTimeline/> : <div></div>}
+
+                        {postData.map((post, index) => {
+                            return(
+                                <PostComponent post={post} key={index}/>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                {/* RIGHT CONTENT */}
+                <Rightbar loggedIn={loggedIn} post={postData} allUserData={allUserData}/>
+            </div>
+        </div>
+    )
+};
+
+export default Profile;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// const Profile = () => {
+//   return (
+//     <div>Profile</div>
+//   )
+// }
+
+// export default Profile
