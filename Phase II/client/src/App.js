298ae5b6afa0a0f1b1930adc4b88a72f6dee2eb4
btjandra15 <btjandra15@gmail.com>
2023-11-28 20:48:13 -0500

    Actually fixed the job posting page lol

--- Phase II/client/src/App.js
+++ Phase II/client/src/App.js
@@ -58,7 +58,7 @@ function App() {
             <Route path='/trending' element={<Trending/>}/> 
 
             {/*Postings Route*/}
-            <Route path='/postings' element={<JobPostings/>}/> 
+            <Route path='/job-postings' element={<JobPostings/>}/> 
 
             {/*Payment Route*/}
             <Route path='/payment' element={<Payment/>}/> 
