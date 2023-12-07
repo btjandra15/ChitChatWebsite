547586933ced9b690362bf6be4b5ed2173e497ee
thesameninja <107506915+thesameninja@users.noreply.github.com>
2023-12-07 10:31:52 -0500

    Added Suggested users to all pages

--- Phase II/client/src/Pages/Profile/Profile.js
+++ Phase II/client/src/Pages/Profile/Profile.js
@@ -176,7 +176,7 @@ const Profile = () => {
           </div>
         </div>
 
-        <Rightbar loggedIn={loggedIn} post={postData} />
+        <Rightbar loggedIn={loggedIn} userData={userData}/>
       </div>
     </div>
   );
